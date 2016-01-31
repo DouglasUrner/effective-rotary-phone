e013eae846b65ee50a99bd793944f1f5def92a03
Gaelan Steele <gbs@canishe.com>
2016-01-31 13:48:54 -0800

    Move app.jsx to react-komposer.

--- code/client/components/layout/app.jsx
+++ code/client/components/layout/app.jsx
@@ -5,6 +5,7 @@
 */
 
 import React from 'react';
+import {composeWithTracker} from 'react-komposer';
 
 App = React.createClass({
   render() {
@@ -15,37 +16,39 @@ App = React.createClass({
   },
 });
 
-// App = React.createClass({
-//   mixins: [ ReactMeteorData ],
-//   getMeteorData() {
-//     return {
-//       loggingIn: Meteor.loggingIn(),
-//       hasUser: !!Meteor.user(),
-//       isPublic( route ) {
-//         return [
-//           'index',
-//           'singlePost',
-//           'tagIndex',
-//           'login',
-//           'recoverPassword',
-//           'resetPassword',
-//           'notFound'
-//         ].indexOf( route ) > -1;
-//       },
-//       canView() {
-//         return this.isPublic( FlowRouter.getRouteName() ) || !!Meteor.user();
-//       }
-//     };
-//   },
-//   getView() {
-//     return this.data.canView() ? this.props.yield : <Login />;
-//   },
-//   render() {
-//     return <div className="app-root">
-//       <AppHeader hasUser={ this.data.hasUser } />
-//       <div className="container">
-//         { this.data.loggingIn ? <Loading /> : this.getView() }
-//       </div>
-//     </div>;
-//   }
-// });
+const App = React.createClass({
+  getView() {
+    return this.props.canView() ? this.props.yield : <Login />;
+  },
+  render() {
+    return <div className="app-root">
+      <AppHeader hasUser={ this.data.hasUser } />
+      <div className="container">
+        { this.data.loggingIn ? <Loading /> : this.getView() }
+      </div>
+    </div>;
+  }
+});
+
+const data = (props, onData) => {
+  onData(null, {
+    loggingIn: Meteor.loggingIn(),
+    hasUser: !!Meteor.user(),
+    isPublic( route ) {
+      return [
+        'index',
+        'singlePost',
+        'tagIndex',
+        'login',
+        'recoverPassword',
+        'resetPassword',
+        'notFound'
+      ].indexOf( route ) > -1;
+    },
+    canView() {
+      return this.isPublic( FlowRouter.getRouteName() ) || !!Meteor.user();
+    }
+  })
+}
+
+export default composeWithTracker(data)(App)
